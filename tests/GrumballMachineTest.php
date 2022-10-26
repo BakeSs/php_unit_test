01f5eb5b2daaaf10879daa1426bab98b3063a4ae
BakeSs <58108248+BakeSs@users.noreply.github.com>
2022-10-26 23:33:57 +0300

    Test code fixes

--- tests/GrumballMachineTest.php
+++ tests/GrumballMachineTest.php
@@ -5,11 +5,9 @@ require 'GrumballMachine.php';
 class GumballMachineTest extends TestCase
 {
     public $gumballInstance;
-    public function setUpGumball()
-    {
-        $this->gumballInstance=new GumballMachine();
-    }
+
     public function testWheel(){
+        $this->gumballInstance=new GumballMachine();
         $this->gumballInstance->setGumballs(92);
         $this->gumballInstance->turnWheel();
         $this->assertEquals(91, $this->gumballInstance->getGumballs());
