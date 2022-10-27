83b52f86b0664b7b5ae2e9e028e2f82faf65c576
BakeSs <58108248+BakeSs@users.noreply.github.com>
2022-10-27 10:07:55 +0300

    New tests added

--- tests/GrumballMachineTest.php
+++ tests/GrumballMachineTest.php
@@ -12,4 +12,16 @@ class GumballMachineTest extends TestCase
         $this->gumballInstance->turnWheel();
         $this->assertEquals(91, $this->gumballInstance->getGumballs());
     }
+
+    public function testSetter(){
+        $this->gumballInstance=new GumballMachine();
+        $this->gumballInstance->setGumballs(92);
+        $this->assertEquals(92, $this->gumballInstance->getGumballs());
+    }
+
+    public function testGetter(){
+        $this->gumballInstance=new GumballMachine();
+        $this->gumballInstance->setGumballs(92);
+        $this->assertEquals(92, $this->gumballInstance->getGumballs());
+    }
 }
